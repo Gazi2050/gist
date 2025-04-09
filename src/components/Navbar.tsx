96dd6afd17a7da96257a6b1d834aaf48a486bb5f
Gazi2050 <gazi45344@gmail.com>
2025-04-09 21:44:13 +0600

    feat:creating auth system

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,34 +6,34 @@ import NavLink from './NavLink';
 const Navbar = () => {
     return (
         <nav className="border-b border-gray-800 px-4 py-4 sticky top-0 z-50 bg-black/95 backdrop-blur-sm">
-            <div className="container mx-auto flex items-center justify-between">
+            <div className="relative container mx-auto flex items-center justify-between">
+                {/* Logo */}
                 <Link href='/' className="flex items-center space-x-2">
                     <Code className="h-7 w-7 text-violet-400" />
                     <span className="text-2xl font-bold">Gist</span>
                 </Link>
-                <div className="hidden md:flex space-x-8">
-                    {/* <NavLink href='/' label='Home' />
-                    <NavLink href='/createGist' label='Create Gist' />
-                    <NavLink href='/yourGist' label='Your Gist' /> */}
+
+                {/* Center Nav Links */}
+                <div className="absolute inset-0 hidden md:flex justify-center items-center space-x-8">
                     <NavLink href='/' label='Home' />
                     <NavLink href='/treading' label='Treading' />
                     <NavLink href='/aboutUs' label='About Us' />
                 </div>
-                <div>
-                    <div className="flex items-center space-x-3">
-                        <Link
-                            href="/"
-                            className="rounded-md border border-violet-600 px-4 py-2 text-white hover:bg-gray-50/9 transition duration-300 font-semibold"
-                        >
-                            Sign In
-                        </Link>
-                        <Link
-                            href="/"
-                            className="rounded-md bg-violet-600 px-4 py-2 text-white hover:bg-violet-500 transition duration-200 font-semibold"
-                        >
-                            Sign Up
-                        </Link>
-                    </div>
+
+                {/* Auth Buttons */}
+                <div className="flex items-center space-x-3">
+                    <Link
+                        href="/"
+                        className="rounded-md border border-violet-600 px-4 py-2 text-white hover:bg-gray-50/9 transition duration-300 font-semibold"
+                    >
+                        Sign In
+                    </Link>
+                    <Link
+                        href="/"
+                        className="rounded-md bg-violet-600 px-4 py-2 text-white hover:bg-violet-500 transition duration-200 font-semibold"
+                    >
+                        Sign Up
+                    </Link>
                 </div>
             </div>
         </nav>
